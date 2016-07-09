a1ccab88a1504e6e79732dbbdcb1e5e84347fdfa
sibuachu <sibuachu@gmail.com>
2016-07-08 21:25:10 -0400

    Hello world test commit

---
+++ helloworld/src/com/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.helloworld;
+
+/**
+ * Created by Cbu on 7/8/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
